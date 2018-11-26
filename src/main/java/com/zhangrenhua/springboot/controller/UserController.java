8bf516aadd815cfd10920682a221a9172da55422
张任华 <zhangrenhua@extracme.com>
2018-11-26 18:18:57 +0800

    提交代码

--- src/main/java/com/zhangrenhua/springboot/controller/UserController.java
+++ src/main/java/com/zhangrenhua/springboot/controller/UserController.java
@@ -8,12 +8,14 @@ import com.zhangrenhua.springboot.entity.User;
 import com.zhangrenhua.springboot.exception.CommonException;
 import com.zhangrenhua.springboot.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -27,12 +29,27 @@ import java.util.Map;
  * @author zhangrenhua
  * @since 2018-11-23
  */
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
     
     @Autowired
     private IUserService userService;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @GetMapping("set/{key}/{value}")
+    public String set(@PathVariable("key")String key,@PathVariable("value") String value) {
+        //注意这里的 key不能为null spring 内部有检验
+        redisTemplate.opsForValue().set(key, value);
+        return key + "," + value;
+    }
+
+    @GetMapping("get/{key}")
+    public String get(@PathVariable("key") String key) {
+        return "key=" + key + ",value=" + redisTemplate.opsForValue().get(key);
+    }
     
     @PostMapping("add")
     public Map<String,String> addUser(@Valid @RequestBody UserReq userReq){
